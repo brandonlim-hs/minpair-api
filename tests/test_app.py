be93043a2c1fbca7756eb1772ba9d2e593e85be4
Brandon Lim <brandonlim.lim@gmail.com>
2019-10-20 17:15:40 +1100

    Add test for different query parameters

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,6 @@
+from urllib.parse import urlencode
 import json
+import pytest
 
 
 def test_index(client):
@@ -6,9 +8,23 @@ def test_index(client):
     assert b"minpair API" in response.data
 
 
-def test_vowel_minpair(client):
-    response = client.get("/vowel?vowels=AE&vowels=EH")
-    assert {
-        "AE": "last",
-        "EH": "less"
-    } in json.loads(response.data.decode("utf-8"))["minimal_pairs"]
+@pytest.mark.parametrize(
+    ("vowels", "pos", "expected_minimal_pairs"),
+    (
+        (["AE", "EH"], [], [{"AE": "bad", "EH": "bed"},
+                            {"AE": "last", "EH": "less"}]),
+        (["AE", "EH"], ["ADJ"], [{"AE": "last", "EH": "less"}]),
+        (["AE", "EH", "IH"], [], [{"AE": "bad", "EH": "bed", "IH": "bid"}]),
+    )
+)
+def test_vowel_minpair(client, vowels, pos, expected_minimal_pairs):
+    query = urlencode({
+        "vowels": vowels,
+        "pos": pos,
+    }, True)
+    response = client.get("/vowel?" + query)
+    minimal_pairs = json.loads(response.data.decode("utf-8"))["minimal_pairs"]
+    assert all(
+        minimal_pair in minimal_pairs
+        for minimal_pair in expected_minimal_pairs
+    )
