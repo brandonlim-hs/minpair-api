e013623cdf451559f6920277f857b3269f4c071c
Brandon Lim <brandonlim.lim@gmail.com>
2019-10-20 13:28:11 +1100

    Add API for vowel minpair

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import minpair
 app = Flask(__name__)
 
@@ -6,3 +6,12 @@ app = Flask(__name__)
 @app.route("/", methods=["GET"])
 def index():
     return "minpair API: " + minpair.__version__
+
+
+@app.route("/vowel", methods=["GET"])
+def vowel_minpair():
+    vowels = request.args.getlist("vowels")
+    pos = request.args.getlist("pos")
+    return {
+        "minimal_pairs": minpair.vowel_minpair(vowels, pos)
+    }
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,5 @@
 from app import app
+import json
 import pytest
 
 
@@ -6,3 +7,12 @@ def test_index():
     app.testing = True
     response = app.test_client().get("/")
     assert b"minpair API" in response.data
+
+
+def test_vowel_minpair():
+    app.testing = True
+    response = app.test_client().get("/vowel?vowels=AE&vowels=EH")
+    assert {
+        "AE": "last",
+        "EH": "less"
+    } in json.loads(response.data.decode("utf-8"))["minimal_pairs"]
